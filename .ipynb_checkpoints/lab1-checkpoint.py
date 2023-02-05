fix normalization for first function

.ipynb_checkpoints/lab1-checkpoint.py
@@ -113,12 +113,13 @@ def normalized_cross_correlation(img, template):
     n_colors = img.shape[2]
     response = np.zeros(shape=(Ho, Wo))
     template_scaled = np.divide(template, np.sum(template))
-    filter_magnitude = np.sqrt(np.sum(np.square(template_scaled)))
+    
+    filter_magnitude = np.linalg.norm(template_scaled)
     
     for i in range(Ho):
         for j in range(Wo):
             input_window = img[i:i + Hk, j:j + Wk]
-            input_window_magnitude = np.sqrt(np.sum(np.square(input_window)))
+            input_window_magnitude = np.linalg.norm(input_window)
             for ti in range(Hk):
                 for tj in range(Wk):
                     for k in range(n_colors):
@@ -227,7 +228,7 @@ def non_max_suppression(response, suppress_range, threshold=None):
             res[i,j] = response[i,j]
             startI = max(maxI - suppress_range(0),0)
             endI = min(maxI + suppress_range(0), h - 1)
-            startJ = max(maxJ - supress_range(1), 0)
+            startJ = max(maxJ - suppress_range(1), 0)
             endJ = min(maxJ - suppress_range(1), h - 1)
             for i in range(startI, endI + 1):
                 for j in range(startJ, endJ + 1):

lab1.py
@@ -113,12 +113,13 @@ def normalized_cross_correlation(img, template):
     n_colors = img.shape[2]
     response = np.zeros(shape=(Ho, Wo))
     template_scaled = np.divide(template, np.sum(template))
-    filter_magnitude = np.sqrt(np.sum(np.square(template_scaled)))
+    
+    filter_magnitude = np.linalg.norm(template_scaled)
     
     for i in range(Ho):
         for j in range(Wo):
             input_window = img[i:i + Hk, j:j + Wk]
-            input_window_magnitude = np.sqrt(np.sum(np.square(input_window)))
+            input_window_magnitude = np.linalg.norm(input_window)
             for ti in range(Hk):
                 for tj in range(Wk):
                     for k in range(n_colors):